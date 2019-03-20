a7032eab1b9db184e2b12e2901e6f54c992b8409
darksidebleat <darksidebleat.12@mail.ru>
2019-03-20 10:03:56 +0700

    sleep

--- lab3.cpp
+++ lab3.cpp
@@ -1,23 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-int i = 0;
-void* thread_func(void *arg) {
- pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
- for (i=0; i < 4; i++) {
- printf("I'm still running!\n");
- sleep(1);
- }
- pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
-#include <stdlib.h>
-#include <stdio.h>
-#include <pthread.h>
+#include <unistd.h>
 int i=0;
-void*thread_func(void*arg){
+void* thread_func(void* arg){
 	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 	for (i=0; i<4; i++) {
 		printf("i'm still running!\n");
-		sleep(1);
+		usleep(1);
 	}
 	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
 	pthread_testcancel();
@@ -25,7 +15,7 @@ void*thread_func(void*arg){
 	}
 int main(int argc, char * argv[]){
 	pthread_t thread;
-	pthread_create(&thread, NULL, thread_func.NULL);
+	pthread_create(&thread, NULL, thread_func,NULL);
 	while(i<1) sleep(1);
 	pthread_cancel(thread);
 	printf("Requested to cancel the thread\n");
